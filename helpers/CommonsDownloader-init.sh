30c5f5d5b228441d7760fa8896840cdb561deb3b
Balázs Orbán <285574+orbanbalage@users.noreply.github.com>
2023-09-01 14:46:18 +0200

    fix python CommonsDownloader

--- helpers/CommonsDownloader-init.sh
+++ helpers/CommonsDownloader-init.sh
@@ -1,11 +1,28 @@
 #!/bin/zsh
 
-pyenv install 2.7.18
+# if (pyenv version | grep -q 2.7.18) then
+#     echo "pyenv already installed"
+#     pyenv version
+# else
+#     pyenv install 2.7.18
+# fi
 
-pyenv local 2.7.18
+# pyenv local 2.7.18
 
-pip install CommonsDownloader
+# pyenv init
 
-/Users/orbanbalage/_sandbox/_websites/szlovakiaiidegenvezetes.sk/helpers/download_from_Wikimedia_Commons
+# pip install pip
 
-download_from_Wikimedia_Commons -l ../erb-list-cz -v
+# uninstalled these previous attempts
+
+### NEED TO BUILD FORK and updated branch for Python3
+# https://github.com/Johnson145/CommonsDownloader/tree/python3_7
+# gh repo clone Johnson145/CommonsDownloader
+# git checkout python3_7
+# had to also add: import urllib.request
+# this is deprecated...:
+# python3 setup.py install
+
+download_from_Wikimedia_Commons --list ../../erb-list-cz.csv --output ../CZ-images -v
+
+download_from_Wikimedia_Commons --list ../../erb-list-sk.csv --output ../SK-images -v
